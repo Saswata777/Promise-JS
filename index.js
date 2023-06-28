0fdd77661b4f7ee7643faa1050d4fa2947f978c7
Saswata <saswatamaitra3.14@gmail.com>
2023-06-28 12:41:03 +0530

    Creaing promise

--- index.js
+++ index.js
@@ -6,3 +6,41 @@ console.log(user);
 user.then(function(data){
             console.log(data);
 });
+
+
+
+
+const cart = ["Shoes", "Pants", "Kurta"];
+
+const promise = createOrder(cart);
+
+promise.then(function (orderId){
+    console.log(orderId);
+});
+
+function createOrder(cart){
+    const pr = new Promise(function(resolve, reject){
+        // Create Order
+        // Validate Cart
+        // Order Id
+        if(!ValidateCart(cart)){
+            const err = new Error("Cart is not valid");
+            reject (err);
+        }
+
+        // logic for Create order
+
+        const orderId = "12345";
+
+        if(orderId){
+            resolve(orderId);
+        }
+
+        
+    });
+    return pr;
+}
+
+function ValidateCart(cart){
+    return true;
+}
